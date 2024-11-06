a07df6b089fa698ce24382c94779e5b10e3253e8
Muhammad Abdullah Shurjeel <99145322+MuhammadAbdullahShurjeel@users.noreply.github.com>
2024-11-06 20:21:36 +0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,10 @@ lua54 'yes'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
+author "_fzzy"
+version "1.0"
+description "Script Made For RSG FRAMEWORK"
+
 
 shared_scripts {
     '@ox_lib/init.lua',
